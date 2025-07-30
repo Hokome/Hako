9da264cc65e22a365795a44f0ac16b42e1a1d7b9
Hokome <m.k.esseghir@protonmail.com>
2025-07-30 16:21:24 +0200

    Menu focus management

--- addons/menu_toolkit/core/menu.gd
+++ addons/menu_toolkit/core/menu.gd
@@ -5,6 +5,9 @@ signal transitioned(bool)
 
 ## Prevent [method MenuStack.back] from hiding this menu.
 @export var is_root: bool = false
+@export var first_focus: Control
 
 func end_transition(show: bool) -> void:
+	if first_focus:
+		first_focus.grab_focus()
 	transitioned.emit(show)
