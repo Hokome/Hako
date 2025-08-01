110b01681f0b589ad1a725376580943d2212373e
Hokome <m.k.esseghir@protonmail.com>
2025-08-01 11:42:15 +0200

    Combat toolkit

---
+++ addons/combat_toolkit/capped_int.gd
@@ -0,0 +1,22 @@
+extends Node
+class_name CappedInt
+
+signal value_changed(new_value: int)
+signal max_value_changed(new_max: int)
+signal value_zero
+
+@export var max_value: int:
+	set(val):
+		max_value = max(0, val)
+		if value > max_value:
+			value = value
+
+var value: int:
+	set(val):
+		value = clampi(val, 0, max_value)
+		value_changed.emit(value)
+		if value == 0:
+			value_zero.emit()
+
+func _init() -> void:
+	value = max_value
